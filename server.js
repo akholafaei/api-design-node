d864036c64d84ee942a56f217501f783b3890bc9
akholafaei <akholafaei@ebay.com>
2018-04-14 10:43:21 -0700

    add solution for step1

--- server.js
+++ server.js
@@ -3,3 +3,16 @@
 // it should then send back jsonData on a GET to /data
 
 var jsonData = {count: 12, message: 'hey'};
+
+var express = require('express');
+
+var app = express();
+app.get('/', function (req, res) {
+    res.sendFile(__dirname, index.html);
+});
+
+app.get('/data', function (req, res) {
+    res.json(jsonData);
+});
+
+app.listen('You are liseting to 3000', 3000);
